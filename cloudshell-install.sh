eaf58dfdfee8e36b441fb31d34ed22953c10e55e
Xing Yahao <keidarcy.1015@gmail.com>
2024-05-01 10:28:00 +0900

    Update cloudshell-install.sh

--- cloudshell-install.sh
+++ cloudshell-install.sh
@@ -14,7 +14,7 @@ bin_dir="$HOME/.local/bin"
 
 [ ! -d "$download_dir" ] && mkdir -p "$download_dir"
 
-curl -o "$download_dir/e1s.tar.gz" -sSLf "https://github.com/keidarcy/e1s/releases/download/v${version}/e1s_${version}_linux_amd64.tar.gz"
+curl -o "$download_dir/e1s.tar.gz" -sSLf "https://github.com/keidarcy/e1s/releases/download/${version}/e1s_${version}_linux_amd64.tar.gz"
 tar -xzf "$download_dir/e1s.tar.gz" -C "$download_dir"
 
 [ ! -d "$bin_dir" ] && mkdir -p "$bin_dir"
